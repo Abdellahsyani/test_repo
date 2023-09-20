b20b917b663e1613d25f0e8776faf0eca07dd569
Abdellahsyani <syaniabdos@gmail.com>
2023-09-20 16:09:00 +0100

    jhe

--- execute.c
+++ execute.c
@@ -1,47 +1,10 @@
-#include "main.h"
+#include "shell.h"
 
 /**
  * excom - function for command execution
  * @argv: argument vector
  *
  */
-void excom(char **argv)
+int excom(char **argv, char **mycomm)
 {
-	char *mycomm = NULL, *lastcom = NULL;
-	pid_t mypid = fork();
 
-	if (argv)
-	{
-		mycomm = argv[0];
-		if (_strcmp(mycomm, "exit") == 0)
-		{
-			exit(127);
-		}
-
-		if (mypid < 0)
-		{
-			perror("Fork failed:");
-			exit(127);
-		}
-		else if (mypid == 0)
-		{
-			lastcom = getmy_loc(mycomm);
-
-			if (lastcom == NULL)
-			{
-				exit(1);
-			}
-			if (execve(lastcom, argv, NULL) == -1)
-			{
-				perror("There is an Error:");
-				free(lastcom);
-				exit(1);
-			}
-		}
-		else
-		{
-			wait(NULL);
-		}
-	}
-	free(lastcom);
-}
